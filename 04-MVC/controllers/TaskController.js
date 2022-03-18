9ed39df36014359494c54e7b57a066a45471c9f2
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-18 09:09:00 -0300

    .

--- 04-MVC/controllers/TaskController.js
+++ 04-MVC/controllers/TaskController.js
@@ -4,6 +4,19 @@ module.exports = class TaskController{
     static createTask(req, res){
         res.render('tasks/create')
     }
+
+    static async createTaskSave(req, res){
+        const { title, description } = req.body;
+        const done = false;
+
+        const task = await Task.create({
+            title,
+            description,
+            done
+        });
+        res.redirect('/tasks');
+    }
+
     static showTask(req, res){
         res.render('tasks/all')
     }
