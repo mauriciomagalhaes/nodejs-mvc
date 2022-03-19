c36d1678b7e617faae9cbfa3d73e76402ccfebd8
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-18 22:57:38 -0300

    .

--- 05-projeto-1/index.js
+++ 05-projeto-1/index.js
@@ -9,14 +9,41 @@ const app = express();
 // Connection to database
 const conn = require('./db/conn');
 
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'))
-app.use(express.json());
-
-//handlebars
+// Handlebars
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+// Middlewares
+app.use(express.urlencoded({ extended: false })); // Recive data from body
+app.use(express.static('public')) // Static files
+app.use(express.json()); // Json
+app.use(flash());
+// Midllwares for sessions
+app.use(session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: function () {},
+        path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 3600000,
+        expires: new Date(Date.now() + 3600000),
+        httpOnly: true,
+    }
+}))
+
+// set seesion to response
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session;
+    }
+
+    next();
+})
 
 // Connection with mysql and start server on port 3000
 conn
