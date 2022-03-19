e20874d7977c4451b1fa0f3d25154e9d88fe423e
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-18 22:18:41 -0300

    .

---
+++ 05-projeto-1/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expnbs = require('express-handlebars');
+const conn = require('./db/conn');
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
+app.use(express.json());
+
+//handlebars
+app.engine('handlebars', expnbs.engine());
+app.set('view engine', 'handlebars');
+
+// listen to port
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+})
