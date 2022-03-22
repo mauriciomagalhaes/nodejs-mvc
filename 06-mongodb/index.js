a347bea875e1f6f327e3538b539f001376ed178e
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-21 22:35:39 -0300

    .

--- 06-mongodb/index.js
+++ 06-mongodb/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const conn = require('./db/conn');
 
+const productsRoutes = require('./routes/productsRoutes');
+
+
 // Handlebars
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -12,5 +15,9 @@ app.set('view engine', 'handlebars');
 // Middlewares
 app.use(express.urlencoded({ extended: true })); // Recive data from body
 app.use(express.json()); // Json
+
+app.use(express.static('public')); // Static files
+
+app.use('/products', productsRoutes);
 
 app.listen(3000)
