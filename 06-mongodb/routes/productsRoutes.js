cce25a4f559ea42741c40752535a1b5a35d93724
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-21 23:14:31 -0300

    .

--- 06-mongodb/routes/productsRoutes.js
+++ 06-mongodb/routes/productsRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const ProductController = require('../controllers/ProductController');
 
 router.get('/', ProductController.showProducts);
+router.get('/create', ProductController.createProduct);
+router.post('/create', ProductController.createProductPost);
+
 
 module.exports = router;
