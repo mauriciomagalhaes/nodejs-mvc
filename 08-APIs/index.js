45cf9e84dc777e44e35ab417f48042ad6bc26ad5
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-24 22:18:09 -0300

    .

--- 08-APIs/index.js
+++ 08-APIs/index.js
@@ -5,9 +5,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// rotas
+// rotas - endpoints
+app.post('/createproduct', (req, res) => {
+    const { name, price } = req.body;
+    //console.log(name, price);
+    if(!name){
+        return res.status(422).json({message: 'O campo nome é obrigatório'});
+        return
+    }
+
+    res.status(201).json({message: `Produto ${name} foi criado com sucesso!`});
+});
+
 app.get('/', (req, res) =>{
-    res.json({ message: 'Primeira rota criada com sucesso' });
+    res.status(200).json({ message: 'Primeira rota criada com sucesso' });
 });
 
 app.listen(3000)
