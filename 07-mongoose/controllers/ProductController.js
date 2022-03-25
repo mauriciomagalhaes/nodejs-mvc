d892af2c258ad9010c9e225ad7a34c199a6d02a3
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-03-24 21:11:15 -0300

    .

--- 07-mongoose/controllers/ProductController.js
+++ 07-mongoose/controllers/ProductController.js
@@ -1,13 +1,10 @@
 const Product = require('../models/Product');
 
 module.exports = class ProductController {
-    // static async showProducts(req, res) {
-    //     const products = await Product.getProducts().then(products => {
-    //         res.render('products/all', { products });
-    //     });
-    //     //console.log(products);
-    //     //res.render('products/all', { products });
-    // }
+    static async showProducts(req, res) {
+         const products = await Product.find().lean();
+        res.render('products/all', { products });
+    }
 
     static createProduct(req, res) {
         res.render('products/create')
@@ -15,44 +12,31 @@ module.exports = class ProductController {
 
     static async createProductPost(req, res) {
         const { name, image, price, description } = req.body;
-
         const product = new Product({name, image, price, description});
-
         await product.save();
-    
         res.redirect('/products');
     }
-//     static async getProduct(req, res) {
-//         const product = await Product.getProductById(req.params.id).then(product => {
-//             res.render('products/product', { product });
-//         });
-//     }
+    
+     static async getProduct(req, res) {
+        const product = await Product.findById(req.params.id).lean();
+        res.render('products/product', { product });
+     }
   
-//     static async removeProduct(req, res) {
-//         await Product.removeProductById(req.params.id).then(() => {
-//             res.redirect('/products');
-//         });
-//     }
-//     static async editProduct(req, res) {
-//         const product = await Product.editProductById(req.params.id).then(product => {
-//             res.render('products/edit', { product });
-//         });
-//     }
+     static async removeProduct(req, res) {
+            await Product.deleteOne({_id: req.params.id});
+            res.redirect('/products');
+     }
+     static async editProduct(req, res) {
+        const product = await Product.findById(req.params.id).lean();
+        res.render('products/edit', { product });         
+     }
 
-// /*     static async updateProduct(req, res) {
-//         const { name, image, price, description } = req.body;
-//         const product = new Product(name, image, price, description);
-//         //console.log(product);
-//         await product.updateProductById(req.body.id)
-//         //await Product.updateProductById(req.body.id, product);
-//         res.redirect('/products');
-//     }
-// */
-//     static async updateProduct(req, res) {
-//         const { id, name, image, price, description } = req.body;
-//         const product = new Product(name, image, price, description);
-//         await product.updateProductById(id);
-//         res.redirect('/products');
-//     }
+     static async updateProduct(req, res) {
+         const { id, name, image, price, description } = req.body;
+         const product = {name, image, price, description};
+         await Product.updateOne({_id: id}, product);
+
+         res.redirect('/products');
+     }
 
 }
--- 07-mongoose/models/Product.js
+++ 07-mongoose/models/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const { schema } = mongoose;
+const { Schema } = mongoose;
 
 const Product = mongoose.model(
-    'Product', new schema({
+    'Product', new Schema({
         name: { type: String, required: true },
         price: { type: Number, required: true },
         description: { type: String, required: true },
         image: { type: String, required: true },       
     }),
 );
-module.exports = Products;
+module.exports = Product;
