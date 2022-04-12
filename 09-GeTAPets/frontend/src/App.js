ef177610be58e4fe3640583b5b72d3052f0dbac3
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-04-12 12:20:27 -0300

    .

--- 09-GeTAPets/frontend/src/App.js
+++ 09-GeTAPets/frontend/src/App.js
@@ -1,21 +1,24 @@
 import  {BrowserRoute as Router, Switch, Route} from 'react-router-dom';
+/* Pages */
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
+import Home from './components/pages/home';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/login'>
-
+          <Login />
         </Route>
         <Route path='/register'>
-
+          <Register />
         </Route>        
         <Route path='/'>
-          
+          <Home />
         </Route>
       </Switch>
     </Router>
   );
 }
-
 export default App;
