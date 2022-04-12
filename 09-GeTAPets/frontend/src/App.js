44ba744fbca4e5cba520c5093106a7d4c6c3d118
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-04-12 16:39:00 -0300

    .

--- 09-GeTAPets/frontend/src/App.js
+++ 09-GeTAPets/frontend/src/App.js
@@ -3,6 +3,7 @@ import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 /* Components */
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
 /* Pages */
 import Login from './components/pages/auth/Login';
@@ -13,13 +14,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-
-      <Routes> {/* switch */}
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />   
-      </Routes>
-      
+      <Container>
+        <Routes> {/* switch */}
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />   
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
---
+++ 09-GeTAPets/frontend/src/components/layout/Container.js
@@ -0,0 +1,9 @@
+import styles from './Container.module.css';
+
+function Container(){
+    return (
+        <main className={styles.container}></main>
+    );
+}
+
+export default Container;
