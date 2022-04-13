0ae99213a318d879c1e13de35fcbab74112f7d44
Mauricio Magalhaes <contato@mauriciommagalhaes.com.br>
2022-04-12 22:59:35 -0300

    .

--- 09-GeTAPets/frontend/src/components/pages/auth/Register.js
+++ 09-GeTAPets/frontend/src/components/pages/auth/Register.js
@@ -1,7 +1,67 @@
+
+import { useState } from 'react';
+
+import Input from '../../form/Input';
+import {Link} from 'react-router-dom';
+
+import styles from '../../form/Form.module.css';
+import userEvent from '@testing-library/user-event';
+
 function Register(){
+
+    const [user, setUser] = useState({});
+ 
+    function handleChange(e){
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
-        <section>
-            <h1>Register</h1>
+        <section className={styles.form_container}>
+            <h1>Registrar</h1>
+            <form >
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite seu nome"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite seu telefone"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu E-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite sua senha"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Confirmação de Senha"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirme sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type='submit' value='Cadastrar' />
+            </form>
+            <p>
+                Já possui uma conta? <Link to="/login">Clique aqui</Link>
+            </p>
         </section>
     );
 }
