e78907bff00c4da002b22d223d0870886660665a
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2022-04-14 16:26:52 -0300

    .

--- 09-GeTAPets/frontend/src/components/pages/auth/Login.js
+++ 09-GeTAPets/frontend/src/components/pages/auth/Login.js
@@ -1,7 +1,35 @@
+import { useState, useContext } from 'react';
+import Input from '../../form/Input';
+
+import styles from '../../form/Form.module.css';
+
+/* Context */
+import { Context } from '../../../context/UserContext';
+
 function Login(){
+
+    function handleChange(e){}
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu E-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="senha"
+                    placeholder="Digite sua senha"
+                    handleOnChange={handleChange}
+                />
+            </form>
+                
         </section>
     );
 }
